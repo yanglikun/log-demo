5f456f4597a520d05b76c595053a3f00bc13c7be
yanglikun <yanglikun@users.noreply.github.com>
2016-08-28 21:26:16 +0800

    添加jcl、jdklog、log4j的工程

---
+++ jcl/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by Administrator on 2016/8/28.
+ */
+public class Main {
+
+    private static Log logger=LogFactory.getLog(Main.class);
+
+    public static void main(String[] args) {
+        logger.debug("JCL log");
+    }
+
+}
